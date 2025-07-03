c9151cec26c62479e0ec2a417f3a29867c06ba1d
Bhargav_Sharma <sreebhargava.kandala@gmail.com>
2025-07-03 12:12:54 +0530

    today

---
+++ React things/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+const user ={
+  name: "Henry Cavill",
+  url: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90
+}
+
+export default function AboutPerson(){
+  return (
+    <>
+      <h1>Name: </h1>
+
+    </>
+  )
+}
