095938d06e75c2ebf8cc4c2cc898e767145867a1
Bhargav_Sharma <sreebhargava.kandala@gmail.com>
2025-07-15 07:03:17 +0530

    b

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -1,7 +1,20 @@
-export default function Header(){
-    return (
-        <>
-            <div className="div"></div>
-        </>
-    )
-}
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <div className="logo">MySite</div>
+      <nav className="nav">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#features">Features</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
